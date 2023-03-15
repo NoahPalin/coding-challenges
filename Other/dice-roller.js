49958b13230176905a8f94cd48a3641b3651c9c1
Noah Palin <noah.m.palin@gmail.com>
2023-03-15 14:47:28 -0400

    Contine implementation.

--- Other/dice-roller.js
+++ Other/dice-roller.js
@@ -1,8 +1,29 @@
 
 
-for (let x = 0; x <= 100; x++) {
+
+const rollFormatter = function (rollCount, dice1, dice2, extra = "") {
+
+  const result = `RollNo : ${rollCount} - Dice1: ${dice1} - Dice2: ${dice2} - Total: 9 - Extra= ${extra}`;
+  console.log(result);
+};
+
+
+for (let rollCount = 0; rollCount <= 100; rollCount++) {
+
+  // Generates and integer from 1 to 6.
   let dice1 = Math.floor(Math.random() * 6) + 1;
   let dice2 = Math.floor(Math.random() * 6) + 1;
+  let total = dice1 + dice2;
 
-  console.log(`Dice 1: ${dice1} Dice 2: ${dice2}`);
+  switch (total) {
+    case (total % 3 === 0 && total % 4 === 0):
+      rollFormatter(rollCount, dice1, dice2, "OreWheat")
+    case (total % 3 === 0):
+      rollFormatter(rollCount, dice1, dice2, "Ore")
+    case (total % 4 === 0):
+      rollFormatter(rollCount, dice1, dice2, "Wheat")
+    case (total % 5 === 0):
+      rollFormatter(rollCount, dice1, dice2, "Sheep")
+  }
+
 };
