0b6692f2e09ffaa98533be087b2dff31c8dd23a1
Noah Palin <noah.m.palin@gmail.com>
2023-03-15 14:29:32 -0400

    Create dice generator.

--- Other/dice-roller.js
+++ Other/dice-roller.js
@@ -1,5 +1,8 @@
 
 
 for (let x = 0; x <= 100; x++) {
-  
-}
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
+
+  console.log(`Dice 1: ${dice1} Dice 2: ${dice2}`);
+};
