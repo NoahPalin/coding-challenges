d7b218db42f36c13141b4e831311a14411effcdf
Noah Palin <noah.m.palin@gmail.com>
2023-03-18 15:15:01 -0400

    Finish dice-roller.

--- Other/dice-roller.js
+++ Other/dice-roller.js
@@ -1,29 +1,35 @@
-
-
-
 const rollFormatter = function (rollCount, dice1, dice2, extra = "") {
 
+  // Formats then prints the result.
   const result = `RollNo : ${rollCount} - Dice1: ${dice1} - Dice2: ${dice2} - Total: 9 - Extra= ${extra}`;
   console.log(result);
 };
 
 
-for (let rollCount = 0; rollCount <= 100; rollCount++) {
+for (let rollCount = 1; rollCount <= 100; rollCount++) {
 
   // Generates and integer from 1 to 6.
   let dice1 = Math.floor(Math.random() * 6) + 1;
   let dice2 = Math.floor(Math.random() * 6) + 1;
   let total = dice1 + dice2;
 
-  switch (total) {
-    case (total % 3 === 0 && total % 4 === 0):
-      rollFormatter(rollCount, dice1, dice2, "OreWheat")
-    case (total % 3 === 0):
-      rollFormatter(rollCount, dice1, dice2, "Ore")
-    case (total % 4 === 0):
-      rollFormatter(rollCount, dice1, dice2, "Wheat")
-    case (total % 5 === 0):
-      rollFormatter(rollCount, dice1, dice2, "Sheep")
-  }
+  // Checks for multiples.
+  if (total % 3 === 0 && total % 4 === 0) {
 
+    // Call helper function.
+    rollFormatter(rollCount, dice1, dice2, "OreWheat");
+  } else if (total % 3 === 0) {
+
+    rollFormatter(rollCount, dice1, dice2, "Ore")
+  } else if (total % 4 === 0) {
+
+    rollFormatter(rollCount, dice1, dice2, "Wheat")
+  } else if (total % 5 === 0) {
+
+    rollFormatter(rollCount, dice1, dice2, "Sheep")
+  } else {
+
+    // Called when total is not a multiple of 3, 4, or 5.
+    rollFormatter(rollCount, dice1, dice2)
+  }
 };
