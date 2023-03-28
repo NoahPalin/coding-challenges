827d399fcefcb826fb912daeb680e29b18ebc936
Noah Palin <noah.m.palin@gmail.com>
2023-03-28 18:45:03 -0400

    Finish coding challenge.

--- LeetCode/reverse-integer.js
+++ LeetCode/reverse-integer.js
@@ -1,3 +1,23 @@
-const reverseInt = function () {
-  
+const reverseInt = function (number) {
+  const numberArray = Array.from(String(number), Number);
+  let result = [];
+  let negativeNum = false;
+
+  if (isNaN(numberArray[0])) {
+    negativeNum = true;
+    numberArray.shift();
+  }
+
+  for (let i = numberArray.length; i >= 0; i--) {
+    result.push(numberArray[i]);
+  }
+
+  if (negativeNum) {
+    return '-' + result.join('');
+  } else {
+    return result.join('');
+  }
 }
+
+console.log(reverseInt(12345));
+console.log(reverseInt(-456));
