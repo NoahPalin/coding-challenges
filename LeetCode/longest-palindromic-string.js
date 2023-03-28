14fed2dfda88f4ce46645fb9e4a105bc27f98aa0
Noah Palin <noah.m.palin@gmail.com>
2023-03-28 18:25:32 -0400

    Start new challenge.

--- LeetCode/longest-palindromic-string.js
+++ LeetCode/longest-palindromic-string.js
@@ -1,6 +1,24 @@
-const longestPalindrome = function(string) {
-  let longest = '';
-  let current = '';
+// INCOMPLETE
+const longestPalindrome = function (string) {
+  const stringArray = string.split('');
 
-  
+  for (let i = 0; i < string.length; i++) {
+    const currentArr = stringArray.splice(i)
+    const currentReverseArr = currentArr.reverse();
+
+    const currentStr = currentArr.join('');
+    const currentReverseStr = currentReverseArr.join('');
+
+    console.log(currentStr);
+    console.log(currentReverseStr);
+
+    // if (current === currentReverse) {
+    //   longest = current;
+    // }
+
+  }
+
+  // console.log(current);
 };
+
+longestPalindrome('ABCDEFG');
