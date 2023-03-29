7f15fdf27e086352f83ed50273a2f2e1c68ba3e3
Noah Palin <noah.m.palin@gmail.com>
2023-03-29 11:40:22 -0400

    Complete max ractangle problem.

---
+++ LeetCode/container-with-most-water.js
@@ -0,0 +1,31 @@
+const containerWithMostWater = (heightArr) => {
+  heightArr.splice(0, 0, 0);
+  let currentMax = 0;
+  let potentialMax = 0;
+
+  for (let i = 1; i < heightArr.length; i++) {
+    for (let j = 1; j < heightArr.length; j++) {
+      let maxHeight = heightArr[i];
+
+      if (maxHeight <= heightArr[j]) {
+
+        potentialMax = maxHeight * (j - i);
+        if (potentialMax > currentMax) {
+          currentMax = potentialMax;
+        }
+      } else if (maxHeight > heightArr[j]) {
+        maxHeight = heightArr[j];
+
+        potentialMax = maxHeight * (j - i);
+        if (potentialMax > currentMax) {
+          currentMax = potentialMax;
+        }
+      }
+
+    }
+  }
+  return currentMax;
+}
+
+console.log(containerWithMostWater([5, 2, 3, 4])); // Largest rectangle possible is a 4x3.
+console.log(containerWithMostWater([1,8,6,2,5,4,8,3,7])); // Largest rectangle possible is a 7x7.
