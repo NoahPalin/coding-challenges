2b206235bf4b5dbf057c16b107fadb6d44bbb438
Noah Palin <noah.m.palin@gmail.com>
2023-03-29 12:35:27 -0400

    Finish merge challenge.

--- LeetCode/merge-sorted-list.js
+++ LeetCode/merge-sorted-list.js
@@ -1,17 +1,26 @@
 const merge = function (list1, list2) {
   let result = [];
-  for (let i = 0; i < list1.length; i++) {
-    for (let j = 0; j < list2.length; j++) {
-      
-      if (list1[i] < list2[j]) {
-        result.push(list1[i]);
-      } else {
-        result.push(list2[i]);
-      }
-      break;
+  while (list1.length > 0 || list2.length > 0) {
+    if (list1[0] < list2[0]) {
+      result.push(list1[0]);
+      list1.splice(0, 1);
+    } else if (list1[0] > list2[0]) {
+      result.push(list2[0]);
+      list2.splice(0, 1);
+    } else if (list1[0] === list2[0]) {
+      result.push(list1[0]);
+      result.push(list2[0]);
+      list1.splice(0, 1);
+      list2.splice(0, 1);
+    } else if (list2.length === 0) {
+      result = result.concat(list1);
+      list1 = [];
+    } else if (list1.length === 0) {
+      result = result.concat(list2);
+      list2 = [];
     }
-    return result;
   }
+  return result;
 }
 
-console.log(merge([1, 2, 3], [2, 5, 6]));
+console.log(merge([1, 2, 3, 7, 9], [1, 5, 6, 6, 8])); // Prints [1, 1, 2, 3, 5, 6, 6, 7, 8, 9]
